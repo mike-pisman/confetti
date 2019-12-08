64799671c2de7d0063952ca4114be5f6e8aeb10d
Mike <peaceman.michael@gmail.com>
2019-12-08 00:16:43 -0800

    v 1.0.4
    Added Sliders

--- confetti.js
+++ confetti.js
@@ -1,10 +1,21 @@
 
-var pieces = [];
-var numberOfPieces = 100;
+var maxParticles = 1000;
+var particles = [];
+
+var s_gravity = 50;
+var s_rotation = 50;
+var s_width = 50;
+var s_height = 50;
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     ctx = canvas.getContext('2d');
+
+    scaleCanvas();
+
+    while (particles.length < maxParticles) {
+        particles.push(new Piece(Math.random() * canvas.width, Math.random() * canvas.height));
+    }
 
     setInterval( function(){ drawConfetti(); }, 1000/60);
 }
@@ -15,62 +26,53 @@ function randomColor () {
 }
 
 function confettiPiece(x, y, width, height, r, c) {
-
-    ctx.fillStyle = c;
-
-    // Calculate Corners of the figure
-    var a = x - width, b = y - height;
-    var c = x + width, d = y - height;
-    var h = x + width, j = y + height;
-    var k = x - width, l = y + height;
-
     ctx.beginPath();
-        ctx.moveTo(Math.cos(r)*(a - x) + Math.sin(r)*(b - y) + x, -Math.sin(r)*(a - x) + Math.cos(r)*(b - y) + y);
-        ctx.lineTo(Math.cos(r)*(c - x) + Math.sin(r)*(d - y) + x, -Math.sin(r)*(c - x) + Math.cos(r)*(d - y) + y);
-        ctx.lineTo(Math.cos(r)*(h - x) + Math.sin(r)*(j - y) + x, -Math.sin(r)*(h - x) + Math.cos(r)*(j - y) + y);
-        ctx.lineTo(Math.cos(r)*(k - x) + Math.sin(r)*(l - y) + x, -Math.sin(r)*(k - x) + Math.cos(r)*(l - y) + y);
-    ctx.fill();
+    ctx.moveTo(x + Math.cos(r)*height, y + Math.sin(r)*height);
+    ctx.lineTo(x - Math.cos(r)*height, y - Math.sin(r)*height);
+    ctx.lineWidth = width;
+    ctx.strokeStyle = c;
+    ctx.stroke();
+}
 
-
+function scaleCanvas() {
+    canvas.width = window.innerWidth * .85;
+    canvas.height = window.innerHeight;
 }
 
 function drawConfetti () {
 
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    scaleCanvas();
 
-    ctx.fillStyle = "#fff";
+    ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-
-    while (pieces.length < numberOfPieces) {
-        pieces.push(new Piece(Math.random() * canvas.width, 0));
+    while (particles.length < maxParticles) {
+        particles.push(new Piece(Math.random() * canvas.width, 0));
     }
-
-    for (var i = 0; i < pieces.length; i++) {
-        var p = pieces[i];
+    for (var i = 0; i < particles.length; i++) {
+        var p = particles[i];
 
         if (p.y > canvas.height + p.height) {
-            pieces.splice(i, 1);
+            particles.splice(i, 1);
             continue;
         }
 
-        p.y += p.gravity * canvas.height / 200;
-        p.x += p.rotationSpeed * 10;
-        p.rotation += p.rotationSpeed;
+        p.y += p.gravity * s_gravity / 100;// * canvas.width / 500;
+        p.x += p.rotationSpeed / 100;
+        p.rotation += p.rotationSpeed * s_rotation / 100;
 
-        confettiPiece(p.x, p.y, p.width, p.height, p.rotation, p.color);
+        confettiPiece(p.x, p.y, p.width * s_width / 100, p.height * s_height / 100, p.rotation, p.color);
     }
 }
 
 function Piece (x, y) {
     this.x = x;
     this.y = y;
-    this.width = Math.random() * 4;
-    this.height = Math.random() * this.width;
-    this.gravity = (Math.random() + 0.3);
+    this.width = Math.random() * 5;
+    this.height = Math.random() * this.width * 2;
+    this.gravity = (Math.random() + 0.5) * 4;
     this.rotation = 0;
-    this.rotationSpeed = (Math.random()*2 - 1) * 0.1;
+    this.rotationSpeed = (Math.random()*2 - ;
     this.color = randomColor();
 }
